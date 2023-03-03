c4c34d576542de4f6246cf129e2ca0c8b80e5168
N Darahaas <ndarahaas@gmail.com>
2023-03-03 07:48:50 +0530

    fixed protect issue

--- Controller/auth.ts
+++ Controller/auth.ts
@@ -1,42 +1,39 @@
-import { Request, Response, NextFunction } from "express";
-import * as jwt from "jsonwebtoken";
-import { db } from "../utils/db";
-import { user } from "../interface";
-import { collections } from "../utils/collections";
-import { ObjectId } from "mongodb";
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { db } from '../utils/db';
+import { user } from '../interface';
+import { collections } from '../utils/collections';
+import { ObjectId } from 'mongodb';
 
 export const protect = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-    try {
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.startsWith("Bearer")
-        ) {
-            let token = req.headers.authorization.split(" ").pop() as string;
-            let userId = jwt.verify(
-                token,
-                process.env.JWT_SECRET as string
-            ) as jwt.JwtPayload;
-            if (userId.id) {
-                let userData = await db
-                    .collection<user>(collections.users)
-                    .findOne({ _id: new ObjectId(userId.id) });
-                if (userData) return next();
-                else throw new Error("User not found");
-            }
-        } else {
-            res.status(401).json({
-                status: "failed",
-                msg: "missing authorization headers",
-            });
-        }
-    } catch (err: unknown) {
-        res.status(500).json({
-            status: "failed",
-            msg: err,
-        });
-    }
+	try {
+		if (
+			req.headers.authorization &&
+			req.headers.authorization.startsWith('Bearer')
+		) {
+			let token = req.headers.authorization.split(' ').pop() as string;
+			let userId = jwt.verify(
+				token,
+				process.env.JWT_SECRET as string
+			) as jwt.JwtPayload;
+			if (userId.id) {
+				let userData = await db
+					.collection<user>(collections.users)
+					.findOne({ _id: new ObjectId(userId.id) });
+				if (userData) next();
+				else throw new Error('User not found');
+			} else throw new Error('Malformed JWT');
+		} else {
+			throw new Error('Missing authorization headers');
+		}
+	} catch (err: unknown) {
+		res.status(500).json({
+			status: 'failed',
+			msg: err,
+		});
+	}
 };
--- Controller/data.ts
+++ Controller/data.ts
@@ -1,132 +1,106 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { collections } from "../utils/collections";
-import { user } from "../interface";
-import { db } from "../utils/db";
-import * as jwt from "jsonwebtoken";
-import { ObjectId } from "mongodb";
-import { getQuestionSet } from "../src/questionsets";
+import { collections } from '../utils/collections';
+import { user } from '../interface';
+import { db } from '../utils/db';
+import * as jwt from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
+import { getQuestionSet } from '../src/questionsets';
 
 export const requestData = async (req: Request, res: Response) => {
-    try {
-        console.log(req.body);
-        let token: any = "";
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.startsWith("Bearer")
-        )
-            token = req.headers.authorization.split(" ").pop();
-        if (!token) throw new Error("Token not found");
-        let userId = jwt.verify(
-            token,
-            process.env.JWT_SECRET as string
-        ) as jwt.JwtPayload;
-        if (userId.id) {
-            let userData = await db
-                .collection<user>(collections.users)
-                .findOne({ _id: new ObjectId(userId.id) });
-            if (userData) {
-                let questionSetData = getQuestionSet(userData.questionSet);
-                let dbName = questionSetData?.name;
-                if (!dbName) throw new Error("Database not found");
-                let data = await db.collection(dbName).find().toArray();
+	try {
+		let token = req.headers.authorization!.split(' ').pop() as string;
 
-                //console.log(data);
-                //if (!data) throw new Error("Data not found");
-                res.status(200).json({
-                    status: "success",
-                    data,
-                });
-            } else throw new Error("User not found");
-        }
-    } catch (err: unknown) {
-        return res.status(500).json({
-            status: "failed",
-            msg: err,
-        });
-    }
+		let userId = jwt.verify(
+			token,
+			process.env.JWT_SECRET as string
+		) as jwt.JwtPayload;
+
+		let userData = await db
+			.collection<user>(collections.users)
+			.findOne({ _id: new ObjectId(userId.id) });
+		if (userData) {
+			let questionSetData = getQuestionSet(userData.questionSet);
+			let dbName = questionSetData!.name;
+			if (!dbName) throw new Error('Database not found');
+			let data = await db.collection(dbName).find().toArray();
+
+			//console.log(data);
+			//if (!data) throw new Error("Data not found");
+			res.status(200).json({
+				status: 'success',
+				data,
+			});
+		} else throw new Error('User not found');
+	} catch (err: unknown) {
+		res.status(500).json({
+			status: 'failed',
+			msg: err,
+		});
+	}
 };
 
 export const requestQuestions = async (req: Request, res: Response) => {
-    try {
-        console.log(req.body);
-        let token: any = "";
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.startsWith("Bearer")
-        )
-            token = req.headers.authorization.split(" ").pop();
-        if (!token) throw new Error("Token not found");
-        let userId = jwt.verify(
-            token,
-            process.env.JWT_SECRET as string
-        ) as jwt.JwtPayload;
-        if (userId.id) {
-            let userData = await db
-                .collection<user>(collections.users)
-                .findOne({ _id: new ObjectId(userId.id) });
-            if (userData) {
-                let questionSetData = getQuestionSet(userData.questionSet);
-                let data = questionSetData?.questions;
+	try {
+		let token = req.headers.authorization!.split(' ').pop() as string;
 
-                //console.log(data);
-                //if (!data) throw new Error("Data not found");
-                res.status(200).json({
-                    status: "success",
-                    data,
-                });
-            } else throw new Error("User not found");
-        }
-    } catch (err: unknown) {
-        return res.status(500).json({
-            status: "failed",
-            msg: err,
-        });
-    }
+		let userId = jwt.verify(
+			token,
+			process.env.JWT_SECRET as string
+		) as jwt.JwtPayload;
+
+		let userData = await db
+			.collection<user>(collections.users)
+			.findOne({ _id: new ObjectId(userId.id) });
+		if (userData) {
+			let questionSetData = getQuestionSet(userData.questionSet);
+			let data = questionSetData?.questions;
+
+			res.status(200).json({
+				status: 'success',
+				data,
+			});
+		} else throw new Error('User not found');
+	} catch (err: unknown) {
+		res.status(500).json({
+			status: 'failed',
+			msg: err,
+		});
+	}
 };
 
 export const submit = async (req: Request, res: Response) => {
-    try {
-        console.log(req.body);
-        let token: any = "";
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.startsWith("Bearer")
-        )
-            token = req.headers.authorization.split(" ").pop();
-        if (!token) throw new Error("Token not found");
-        let userId = jwt.verify(
-            token,
-            process.env.JWT_SECRET as string
-        ) as jwt.JwtPayload;
-        if (userId.id) {
-            let userData = await db
-                .collection<user>(collections.users)
-                .findOne({ _id: new ObjectId(userId.id) });
-            if (userData) {
-                // let questionSetData = getQuestionSet(userData.questionSet);
-                // let dbName = questionSetData?.name;
-                // if (!dbName) throw new Error("Database not found");
-                let answers = req.body.answers;
-                console.log(answers);
-                let result = await db
-                    .collection("userSubmission")
-                    .updateOne(
-                        { _id: userId.id },
-                        { $set: { answers: answers } },
-                        { upsert: true }
-                    );
-                console.log(result);
-                res.status(200).json({
-                    status: "success",
-                    data: result,
-                });
-            }
-        }
-    } catch (err: unknown) {
-        return res.status(500).json({
-            status: "failed",
-            msg: err,
-        });
-    }
+	try {
+		let token = req.headers.authorization!.split(' ').pop() as string;
+
+		let userId = jwt.verify(
+			token,
+			process.env.JWT_SECRET as string
+		) as jwt.JwtPayload;
+
+		let userData = await db
+			.collection<user>(collections.users)
+			.findOne({ _id: new ObjectId(userId.id) });
+		if (userData) {
+			let { answers } = req.body;
+
+			let result = await db
+				.collection('userSubmission')
+				.updateOne(
+					{ _id: userId.id },
+					{ $set: { answers: answers } },
+					{ upsert: true }
+				);
+
+			res.status(200).json({
+				status: 'success',
+				data: result,
+			});
+		} else throw new Error('User not found');
+	} catch (err: unknown) {
+		return res.status(500).json({
+			status: 'failed',
+			msg: err,
+		});
+	}
 };
